3922ef648bbe155b3ccd38d2da0e9021608add2b
Murad Hasanov <muradhov23@gmail.com>
2022-11-29 20:53:40 +0400

    created 2 tabs

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,32 +1,32 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
-  static values = {
-    apiKey: String,
-    markers: Array
-  }
+// export default class extends Controller {
+//   static values = {
+//     apiKey: String,
+//     markers: Array
+//   }
 
-  connect() {
-    mapboxgl.accessToken = this.apiKeyValue
-    #addMarkersToMap() {
-      this.markersValue.forEach((marker) => {
-        const popup = new mapboxgl.Popup().setHTML(marker.info_window)
-          .setLngLat([ marker.lng, marker.lat ])
-          .setPopup(popup)
-          .addTo(this.map)
-      })
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
+//   connect() {
+//     mapboxgl.accessToken = this.apiKeyValue
+//     #addMarkersToMap() {
+//       this.markersValue.forEach((marker) => {
+//         const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+//           .setLngLat([ marker.lng, marker.lat ])
+//           .setPopup(popup)
+//           .addTo(this.map)
+//       })
+//     this.#addMarkersToMap()
+//     this.#fitMapToMarkers()
 
-    this.map = new mapboxgl.Map({
-      container: this.element,
-      style: "mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb"
-    })
-  }
-}
-  #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-  }
-}
+//     this.map = new mapboxgl.Map({
+//       container: this.element,
+//       style: "mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb"
+//     })
+//   }
+// }
+//   #fitMapToMarkers() {
+//     const bounds = new mapboxgl.LngLatBounds()
+//     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+//     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+//   }
+// }
