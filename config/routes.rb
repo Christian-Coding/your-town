09b488d89dbd1fc69ff2856cdac7afc9f7df4306
Abbas Nazarov <abbas.nazarov@gmail.com>
2022-11-30 01:00:35 +0400

    dashboard frontend update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "dashboard", to: "pages#dashboard"
   root to: "pages#home"
   resources :cities, only: [:index, :new, :create] do
     resources :projects, only: [:new, :create]
