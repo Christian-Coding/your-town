883ab85531278815ef802e9625011a5ca11fac0c
Christian Krawczyk <c.p.krawczyk@gmail.com>
2022-11-29 22:27:08 +0100

    further fine-tuning on look and feel of simple_form

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,7 @@ class Project < ApplicationRecord
   has_many_attached :photos
   belongs_to :user
   belongs_to :city
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
 end
