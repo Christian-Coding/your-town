b4e6395e05da70c7a71e754eafe9b1fecf7ecda2
Ross Hunter <rhunter82@hotmail.com>
2022-11-30 13:06:38 +0100

    re routed everthing, city table deactivated

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,8 +1,8 @@
 class City < ApplicationRecord
   # belongs_to :user
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
-  has_many :projects
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
+  # has_many :projects
 
   include PgSearch::Model
   pg_search_scope :search_by_city,
