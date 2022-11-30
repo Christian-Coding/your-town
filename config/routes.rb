9166efab76e5ea8c82a3a7c0a08c78e14e38618a
Ross Hunter <rhunter82@hotmail.com>
2022-11-30 17:31:36 +0100

    popup linked to show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get "dashboard", to: "pages#dashboard"
   root to: "pages#home"
-
-  get "dashboard", to: "pages#dashboard"
-    resources :projects, only: [:index, :new, :create, ;show]
-
+    resources :projects, only: [:index, :new, :create, :show]
 end
