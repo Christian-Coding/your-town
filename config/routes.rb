95654047557d9daedeaf5f5f91c7aa368a5f48ba
Ross Hunter <rhunter82@hotmail.com>
2022-12-02 12:50:04 +0100

    vote logic- no minus downvotes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "dashboard", to: "pages#dashboard"
-    resources :projects, only: [:index, :new, :create, :show, :update, :edit]
-      member do
-        post "upvote", to: "projects#upvote"
-        post "downvote", to: "projects#downvote"
-      end
+  resources :projects, only: [:index, :new, :create, :show, :update, :edit] do
+    member do
+      post "upvote", to: "projects#upvote"
+      post "downvote", to: "projects#downvote"
+    end
+  end
     delete "projects/:id", to: "projects#destroy", as: :delete_project
     resources :chatrooms, only: :show do
       resources :messages, only: :create
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_113931) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_151933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_113931) do
     t.string "district"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
   create_table "messages", force: :cascade do |t|
