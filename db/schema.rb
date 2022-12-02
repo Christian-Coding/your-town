4dc4307b8e85b9b10391c4c9320c20c317219f4a
Christian Krawczyk <c.p.krawczyk@gmail.com>
2022-12-02 15:00:17 +0100

    cloudinary upload fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_113931) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_103827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_113931) do
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.string "district"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
