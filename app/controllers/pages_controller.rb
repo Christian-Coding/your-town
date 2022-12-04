0f7fd3f0dd0abfa332a1e172f74ecf526f34f08b
Christian Krawczyk <c.p.krawczyk@gmail.com>
2022-12-04 14:56:38 +0100

    destroy message. upvote in dashboard. some layout to show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,16 +14,4 @@ class PagesController < ApplicationController
 
     redirect_to dashboard_path
   end
-
-  def upvote
-    @project = Project.find(params[:id])
-    @project.upvote_by current_user
-    redirect_back fallback_location: root_path
-  end
-
-  def downvote
-    @project = Project.find(params[:id])
-    @project.downvote_by current_user
-    redirect_back fallback_location: root_path
-  end
 end
