6b220f9829fc5101857083df419e4f09572f2fba
Christian Krawczyk <c.p.krawczyk@gmail.com>
2022-12-04 14:35:57 +0100

    little change on dashboard and show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
-  def home
-    @projects = City.all
-  end
-
   def dashboard
     @projects = current_user.projects
     if params[:query].present?
@@ -17,5 +13,17 @@ class PagesController < ApplicationController
     @project.destroy
 
     redirect_to dashboard_path
+  end
+
+  def upvote
+    @project = Project.find(params[:id])
+    @project.upvote_by current_user
+    redirect_back fallback_location: root_path
+  end
+
+  def downvote
+    @project = Project.find(params[:id])
+    @project.downvote_by current_user
+    redirect_back fallback_location: root_path
   end
 end
