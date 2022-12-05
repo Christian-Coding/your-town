fc3214593234d43cd207bdb5a3361aeac12f4f32
Murad Hasanov <muradhov23@gmail.com>
2022-12-05 18:47:25 +0400

    completed chatroom

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,3 +2,7 @@ class Message < ApplicationRecord
   belongs_to :chatroom
   belongs_to :user
 end
+
+def sender?(a_user)
+  user.id == a_user.id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_151933) do
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.string "district"
-    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
